bdd1e7f94b6267fc7dc5206fda2a0fa50f52efd5
monysary <sary.mony@gmail.com>
2022-10-25 10:58:47 -0700

    Added topic randomizer

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,24 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopics = topics[Math.floor(Math.random()*topics.length)];
 
-/*
-if (topics === "HTML") {
-    console.log("Let's study HTML!");
-} else if (topics === 'CSS') {
-    console.log("Let's study CSS");
-} else if (topics === 'Git') {
-    console.log("Let's study Git!");
-} else if (topics === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log("Please try again!")
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
-*/
 
-var topics = ["HTML", "CSS", "Git", "JavaScript"];
-for (var z = 0; z < topics.length; z++) {
-    console.log(topics[z]);
+function selectTopics() {
+    if (randomTopics === "HTML") {
+        console.log("Let's study HTML!");
+    } else if (randomTopics === "CSS") {
+        console.log("Let's study CSS");
+    } else if (randomTopics === "Git") {
+        console.log("Let's study Git!");
+    } else if (randomTopics === "JavaScript") {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log("Please try again!")
+    }
 }
 
 /*
@@ -25,3 +27,10 @@ for(var x = 0; x < shapes.length; x++) {
     console.log(shapes[x]);
 }
 */
+
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+console.log("");
+
+console.log("Which topic should we study first?");
+selectTopics();
